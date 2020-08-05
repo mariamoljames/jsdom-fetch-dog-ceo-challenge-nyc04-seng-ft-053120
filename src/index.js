46a4117d154c305633208bdeeca58a520edd48c9
mariamoljames <mariamol.james@gmail.com>
2020-08-05 13:01:00 -0400

    dogCEO

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+window.addEventListener('DOMContentLoaded', function(evt) {
+ fetch(imgUrl)
+ .then(res => res.json() )
+ .then((dogObject) => {
+    turnDogIntoHTML(dogObject)
+ })
+})
+
+let turnDogIntoHTML = (dogObject) => {
+    let imgArray = dogObject["message"]
+    let blankUl = document.createElement("ul")
+    for (let img of imgArray) {
+        let blankLi = document.createElement("li")
+        let blankImg = document.createElement("img")
+        blankImg.src = img
+        blankLi.append(blankImg)
+        blankUl.append(blankLi)
+    }
+    document.innerHTML = blankUl
+}
