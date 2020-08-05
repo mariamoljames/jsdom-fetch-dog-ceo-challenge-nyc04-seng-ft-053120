27949454d0f65ff8b23e14805c6ea0878c16d4c8
vuonga1103 <vuonga1103@yahoo.com>
2020-08-05 13:53:47 -0400

    completed all challenges

--- src/index.js
+++ src/index.js
@@ -1,24 +1,65 @@
 // console.log('%c HI', 'color: firebrick')
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const dogImageContainer = document.querySelector("#dog-image-container");
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+const dogBreedUl = document.querySelector("#dog-breeds");
+const breedDropdown = document.querySelector("#breed-dropdown");
+
 
 window.addEventListener('DOMContentLoaded', function(evt) {
- fetch(imgUrl)
- .then(res => res.json() )
- .then((dogObject) => {
-    turnDogIntoHTML(dogObject)
- })
+  fetch(imgUrl)
+    .then(res => res.json() )
+    .then((dogObject) => {
+        turnDogIntoHTML(dogObject)
+    })
+
+    fetch(breedUrl)
+        .then(res => res.json() )
+        .then((breedObject) => {
+            addBreedsToPage(breedObject)
+        })
+
 })
 
 let turnDogIntoHTML = (dogObject) => {
-    let imgArray = dogObject["message"]
-    let blankUl = document.createElement("ul")
+    let imgArray = dogObject["message"];
+
     for (let img of imgArray) {
-        let blankLi = document.createElement("li")
-        let blankImg = document.createElement("img")
-        blankImg.src = img
-        blankLi.append(blankImg)
-        blankUl.append(blankLi)
+        let blankDiv = document.createElement("div");
+        let blankImg = document.createElement("img");
+        blankImg.src = img;
+        blankDiv.append(blankImg);
+        dogImageContainer.append(blankDiv);
     }
-    document.innerHTML = blankUl
 }
+
+let addBreedsToPage = (breedObject) => {
+    let breedNameArray = Object.keys(breedObject["message"]);
+
+    for (let breedName of breedNameArray) {
+        let blankLi = document.createElement("li");
+        blankLi.innerText = breedName;
+        dogBreedUl.append(blankLi);
+    }
+}
+
+dogBreedUl.addEventListener('click', function(evt) {
+    if (evt.target.tagName === "LI") {
+       evt.target.style.color = "blue";
+    }
+})
+
+breedDropdown.addEventListener('change', function(evt) {
+    let letter = evt.target.value;
+    let allBreedNames = document.querySelectorAll("ul#dog-breeds li");
+
+    for (let breedName of allBreedNames) {
+        let firstLetter = breedName.innerText[0];
+        if (firstLetter !== letter) {
+            breedName.hidden = true;
+        } else {
+            breedName.hidden = false;
+        }
+    }
+})
